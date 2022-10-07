c9433b38ec859a51c38a2ef591142bf2a8ba097d
Rediat <yaidercc@gmail.com>
2022-10-07 06:33:33 -0500

    first commit

---
+++ src/Trivia.jsx
@@ -0,0 +1,8 @@
+
+const Trivia = () => {
+  return (
+    <div>Trivia</div>
+  )
+}
+
+export default Trivia;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/normalice.css';
+import './styles/index.scss';
+import Trivia from './Trivia';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Trivia />
+  </React.StrictMode>
+)
